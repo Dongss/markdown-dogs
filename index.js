f715d0619d271e808bc55aad93e718be41848c1d
Shawn <dongsoso@sina.com>
2015-11-13 19:03:06 +0800

    Add tab handle in textarea

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// handle tab intextarea\
+function handleTab() {
+    var textareas = document.getElementsByTagName('textarea');
+    var count = textareas.length;
+    for (var i = 0; i < count; i++) {
+        textareas[i].onkeydown = function(e) {
+        if (e.keyCode == 9 || e.which == 9) {
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+1; 
+        }
+    }
+}
+}
+
+handleTab();
+
 var md = new Vue({
     el: '#editor',
     data: {
